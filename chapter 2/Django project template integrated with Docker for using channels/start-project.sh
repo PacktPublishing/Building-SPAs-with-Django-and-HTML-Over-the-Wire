c1938398984bf1381d230ff2f2e0200a0f9a489a
Andros Fenollosa <andros@fenollosa.email>
2022-01-09 22:51:58 +0100

    Add template chapter 2

---
+++ chapter 2/Django project template integrated with Docker for using channels/start-project.sh
@@ -0,0 +1,6 @@
+# Create the 'hello-word' project
+django-admin startproject hello_world .
+# Create a folder to host the future App with the name 'simple-app'
+mkdir -p app/simple_app
+# Create the 'simple-app' App
+django-admin startapp simple_app app/simple_app
