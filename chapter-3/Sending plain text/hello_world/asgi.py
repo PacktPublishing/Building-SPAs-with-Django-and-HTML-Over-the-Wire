e1009a00e3bfeb0057de6112b049c28513861fac
Andros Fenollosa <andros@fenollosa.email>
2022-02-09 22:06:35 +0100

    Add new examples

---
+++ chapter-3/Sending plain text/hello_world/asgi.py
@@ -0,0 +1,20 @@
+# hello_world/asgi.py
+import os
+from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.urls import re_path
+from app.simple_app.consumers import EchoConsumer
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hello_world.settings')
+
+application = ProtocolTypeRouter({
+    # Django's ASGI application to handle traditional HTTP requests
+    "http": get_asgi_application(),
+    # WebSocket handler
+    "websocket": AuthMiddlewareStack(
+        URLRouter([
+            re_path(r"^ws/echo/$", EchoConsumer.as_asgi()),
+        ])
+    ),
+})
