e11d89a70b208b41a6f3d11a06e3cd431b4195b4
Andros Fenollosa <andros@fenollosa.email>
2022-02-09 20:56:25 +0100

    Example send data

--- chapter-3/app/simple_app/consumers.py
+++ chapter-3/app/simple_app/consumers.py
@@ -1,5 +1,7 @@
 # app/simple_app/consumers.py
 from channels.generic.websocket import WebsocketConsumer
+from datetime import datetime
+import time
 
 class EchoConsumer(WebsocketConsumer):
 
@@ -14,8 +16,11 @@ class EchoConsumer(WebsocketConsumer):
 
     def disconnect(self, close_code):
         """Event when client disconnects"""
+        self.run_time = False
         pass
 
     def receive(self, text_data):
         """Event when data is received"""
-        pass
+
+        # Send current the time
+        self.send(text_data=str(datetime.now().strftime("%H:%M:%S")))
