cb6ca779988f4800aea14a22ba7d9225eec6b08d
Andros Fenollosa <andros@fenollosa.email>
2022-02-13 15:24:46 +0100

    Update code

--- chapter-3/Sending JSON/app/simple_app/consumers.py
+++ chapter-3/Sending JSON/app/simple_app/consumers.py
@@ -3,6 +3,8 @@ from channels.generic.websocket import WebsocketConsumer
 from datetime import datetime
 import time
 import threading
+from random import randint
+from channels.generic.websocket import JsonWebsocketConsumer
 
 class EchoConsumer(WebsocketConsumer):
 
@@ -29,5 +31,42 @@ class EchoConsumer(WebsocketConsumer):
         pass
 
     def receive(self, text_data):
+        """Event when data is received"""
+        pass
+
+
+class BingoConsumer(JsonWebsocketConsumer):
+
+    def connect(self):
+        self.accept()
+        ## Send numbers to client
+        # Generates numbers 5 random numbers, approximately, between 1 and 10
+        random_numbers = list(set([randint(1, 10) for _ in range(5)]))
+        message = {
+            'action': 'New ticket',
+            'ticket': random_numbers
+        }
+        self.send_json(content=message)
+
+        ## Send balls
+        def send_ball(self):
+            while True:
+                # Send message to client
+                random_ball = randint(1, 10)
+                message = {
+                    'action': 'New ball',
+                    'ball': random_ball
+                }
+                self.send_json(content=message)
+                # Sleep for 1 second
+                time.sleep(1)
+
+        threading.Thread(target=send_ball, args=(self,)).start()
+
+    def disconnect(self, close_code):
+        """Event when client disconnects"""
+        pass
+
+    def receive_json(self, data):
         """Event when data is received"""
         pass
--- chapter-3/Sending JSON/app/simple_app/views.py
+++ chapter-3/Sending JSON/app/simple_app/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html', {})
+
+def bingo(request):
+    return render(request, 'bingo.html', {})
