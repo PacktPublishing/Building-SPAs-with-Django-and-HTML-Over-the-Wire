a08db7d88e989088dc55360f3e51edb4a19a4222
Andros Fenollosa <andros@fenollosa.email>
2022-03-05 13:57:52 +0100

    Update

--- chapter-4/social-network_step_1/static/js/index.js
+++ chapter-4/social-network_step_1/static/js/index.js
@@ -1,5 +1,5 @@
 // Connect to WebSockets server (EchoConsumer)
-const myWebSocket = new WebSocket("ws://{{ request.get_host }}/ws/social-network/");
+const myWebSocket = new WebSocket(`${document.body.dataset.scheme === 'http' ? 'ws' : 'wss'}://${ document.body.dataset.host }/ws/social-network/`);
 
 // Event when a new message is received by WebSockets
 myWebSocket.addEventListener("message", (event) => {
