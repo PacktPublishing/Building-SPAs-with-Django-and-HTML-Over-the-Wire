91a34f975785a1019bbed454df43822b866df097
Andros Fenollosa <andros@fenollosa.email>
2022-03-06 16:12:04 +0100

    Update consumers.py

--- chapter-4/social-network_step_2/app/website/consumers.py
+++ chapter-4/social-network_step_2/app/website/consumers.py
@@ -10,17 +10,30 @@ class SocialNetworkConsumer(JsonWebsocketConsumer):
     max_messages_per_page = 5
 
     def connect(self):
+        """Event when client connects"""
+        # Accept the connection
         self.accept()
+        # Assign the Broadcast group
         async_to_sync(self.channel_layer.group_add)(self.room_name, self.channel_name)
+        # Send you all the messages stored in the database.
         self.send_list_messages()
 
     def disconnect(self, close_code):
         """Event when client disconnects"""
+        # Remove from the Broadcast group
         async_to_sync(self.channel_layer.group_discard)(self.room_name, self.channel_name)
 
     def receive_json(self, data_received):
-        """Event when data is received"""
+        """
+            Event when data is received
+            All information will arrive in 2 variables:
+            'action', with the action to be taken
+            'data' with the information
+        """
+
+        # Get the data
         data = data_received['data']
+        # Depending on the action we will do one task or another.
         match data_received['action']:
             case 'add message':
                 # Add message to database
@@ -50,6 +63,15 @@ class SocialNetworkConsumer(JsonWebsocketConsumer):
                 self.send_list_messages()
 
 
+    def send_html(self, event):
+        """Event: Send html to client"""
+        data = {
+            'selector': event['selector'],
+            'html': event['html'],
+        }
+        self.send_json(data)
+
+
     def send_list_messages(self, page=1):
         """Send list of messages to client"""
         # Filter messages to the current page
@@ -65,13 +87,6 @@ class SocialNetworkConsumer(JsonWebsocketConsumer):
             }
         )
 
-    def send_html(self, event):
-        """Event: Send html to client"""
-        data = {
-            'selector': event['selector'],
-            'html': event['html'],
-        }
-        self.send_json(data)
 
     def open_edit_page(self, id):
         """Send the form to edit the message"""
