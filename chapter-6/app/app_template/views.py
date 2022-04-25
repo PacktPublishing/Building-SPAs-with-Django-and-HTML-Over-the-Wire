b7a8b0d7bd9a602e179419549db31e8e7223ce72
Andros Fenollosa <andros@fenollosa.email>
2022-04-25 17:05:45 +0200

    Fix bugs

--- chapter-6/app/app_template/views.py
+++ chapter-6/app/app_template/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from .forms import LoginForm, SignupForm
 from django.contrib.auth.decorators import login_required
 
